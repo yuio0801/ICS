b226e0113da9750a9be9553ee5c62ae13223694c
yuio0801 <2632656591@qq.com>
2023-12-18 23:54:24 +0800

    Lab

---
+++ lab03/example.d
@@ -0,0 +1,11 @@
+
+sample.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 b1 db 81 69       	mov    $0x6981dbb1,%eax
+   5:	8d 6c 24 3c          	lea    0x3c(%esp),%ebp
+   9:	68 2d 8e 04 08       	push   $0x8048e2d
+   e:	c3                   	ret
